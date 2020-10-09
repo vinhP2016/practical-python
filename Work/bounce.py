94700b167818a142136460a90e1a51c048ff8c92
Vinh Phan <quangvinh.g.phan@gmail.com>
2020-10-08 22:10:54 -0700

    Solution for lab 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,41 @@
 # bounce.py
 #
 # Exercise 1.5
+"""
+bill_thickness = 0.11 * 0.001 # Meters (0.11 mm)
+sears_height = 442 # Height (meters)
+num_bills = 1
+day = 1
+
+while num_bills * bill_thickness < sears_height:
+    print(day, num_bills, num_bills * bill_thickness)
+    day = day + 1
+    num_bills = num_bills * 2
+
+print('Number of days', day)
+print('Number of bills', num_bills)
+print('Final height', num_bills * bill_thickness)
+
+bash % python3 sears.py
+1 1 0.00011
+2 2 0.00022
+3 4 0.00044
+4 8 0.00088
+5 16 0.00176
+6 32 0.00352
+...
+21 1048576 115.34336
+22 2097152 230.68672
+Number of days 23 
+Number of bills 4194304 
+Final height 461.37344
+
+
+"""
+
+height = 100 #intial height
+
+for x in range(1,11):
+	new_height = 3/5 * height
+	print(x, round(new_height,4))
+	height = new_height
