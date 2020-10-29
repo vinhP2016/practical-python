518736f09b20be3ca89e2050ee3fbd5f71b277c8
Vinh Phan <quangvinh.g.phan@gmail.com>
2020-10-29 11:22:19 -0700

    Solution for 1.33

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,13 +1,29 @@
 # pcost.py
 #
 # Exercise 1.27
-f = open('Data/portfolio.csv', 'rt')
-headers = next(f)
-total = 0
-for line in f:
-	row = line.split(',')
-	numShares = int(row[1])
-	priceOfShares = float(row[2])
-	total += numShares * priceOfShares
+import csv
+import sys
+def portfolio_cost(filename):
+    total = 0.0
 
-print('Total ', total)
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            try:	
+                numShares = int(row[1])
+                priceOfShares = float(row[2])
+                total += numShares * priceOfShares
+            except ValueError:
+                print("Invalid Literal", row)
+    return total
+
+## cost = portfolio_cost('Data/portfolio.csv')
+if len(sys.argv) == 2:
+	filename = sys.argv[1]
+else:
+	filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+
+print('Total cost: ', cost)
